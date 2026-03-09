072db8c0e2fe05b6e4488f0b4adcb9808e6d0c65
Lucas Spitzer <spitzer.lucas@proton.me>
2026-03-09 01:14:01 +0100

    Comment import bouton hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
+// import { Button } from "@/components/ui/button"
 import { useEffect, useState } from "react"
 
 export function Hero() {
